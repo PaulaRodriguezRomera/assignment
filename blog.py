34ba63d23b6291217e7544113ccfc266d9de0412
Paula Rodriguez Romera <prodrig3@ed.ac.uk>
2023-11-13 11:41:08 +0000

    main routes created and custom error pages

--- blog.py
+++ blog.py
@@ -1,6 +1,27 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
+
+# Create a Flask instance
 app = Flask(__name__)
 
-@app.route('/index')
+
+# Create a route decorator
+@app.route('/')
 def index():
-    return 'Default root'
+    return render_template("index.html")
+
+# localhost/user/name
+@app.route('/user/<name>')
+def user(name):
+    return render_template("user.html", user_name=name)
+
+# Custom Error Pages
+
+# Invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+# Internal Server Error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
